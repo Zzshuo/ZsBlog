93dad39351bb65641675914e8313840fdd905cb7
zhengshuo <694814357@qq.com>
2019-03-01 17:34:15 +0800

    init

--- blog-web/src/main/java/com/zs/blog/config/WebAppConfig.java
+++ blog-web/src/main/java/com/zs/blog/config/WebAppConfig.java
@@ -10,10 +10,4 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class WebAppConfig implements WebMvcConfigurer {
 
-    /** 静态资源处理 **/
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //重新指定spring boot的静态资源处理前缀
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-    }
 }
