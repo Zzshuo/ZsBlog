01b50fa592f9092fe52c8c99e4ff972b749ac24f
zhengshuo <694814357@qq.com>
2019-03-20 16:12:55 +0800

    delete errorlog

--- vue-element-admin/src/router/index.js
+++ vue-element-admin/src/router/index.js
@@ -229,21 +229,6 @@ export const asyncRouterMap = [
       }
     ]
   },
-
-  {
-    path: '/error-log',
-    component: Layout,
-    redirect: 'noredirect',
-    children: [
-      {
-        path: 'log',
-        component: () => import('@/views/errorLog/index'),
-        name: 'ErrorLog',
-        meta: { title: 'errorLog', icon: 'bug' }
-      }
-    ]
-  },
-
   {
     path: '/excel',
     component: Layout,
